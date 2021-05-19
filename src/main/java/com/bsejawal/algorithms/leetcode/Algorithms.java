cdfb3e912a750135260cfd963ff379c30bb355a3
Bhesh Sejawal <bsejawal@Bheshs-MacBook-Pro.local>
2021-05-19 15:27:38 -0500

    algorithm to reverse integer

---
+++ src/main/java/com/bsejawal/algorithms/leetcode/Algorithms.java
@@ -0,0 +1,22 @@
+package com.bsejawal.algorithms.leetcode;
+
+public class Algorithms {
+    /**
+     * https://leetcode.com/problems/reverse-integer/
+     * @param n
+     * @return reversed integer
+     */
+    private  int reverseInteger(int n) {
+
+        int reversed = 0 ;
+        while(n!=0){
+            int tempReversed = (reversed * 10) + (n%10);
+            if(tempReversed/10 != reversed)
+                return 0;
+            reversed = tempReversed;
+            n = n/10;
+
+        }
+        return reversed;
+    }
+}
