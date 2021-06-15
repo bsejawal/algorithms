ebcfb188a7433cf60b5984907177f19addb9285a
Bhesh Sejawal <bsejawal@Bheshs-MacBook-Pro.local>
2021-06-14 19:59:01 -0500

    ResereStringPreserveSpace

--- src/main/java/com/bsejawal/algorithms/random/RandomAlgorithm.java
+++ src/main/java/com/bsejawal/algorithms/random/RandomAlgorithm.java
@@ -2,22 +2,36 @@ package com.bsejawal.algorithms.random;
 
 
 public class RandomAlgorithm {
+    public static void main(String[] args) {
+//        String input="abc de ";
+//        String output="edc ba ";
+        String input=" a b c ";
+        String output=" c b a ";
+        System.out.println(new RandomAlgorithm().reverseStringPreserveSpace(input).equals(output));
+    }
 
     public String reverseStringPreserveSpace(String s){
         char[] input = s.toCharArray();
         char[] result = new char[input.length];
-        int j=0;
-        for(int i=input.length-1; i>=0; i--){
-            if(input[j] == ' ') {
-                result[j++] = ' ';
-                i++;
-                continue;
-            }
-            if(input[i] == ' ')
-                continue;
 
-         result[j++] = input[i];
+
+        for(int i=0; i<input.length; i++)
+            if(input[i] == ' ') result[i] = ' ';
+
+        int j=input.length-1;
+        for(int i=0; i<input.length; i++) {
+            if (input[i] != ' ') {
+                if (result[j]==' ') {
+                    j--;
+                }
+                result[j] = input[i];
+                j--;
+
+            }
         }
+
+
         return String.valueOf(result);
     }
+
 }
