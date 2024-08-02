31763ce2928548bcd6402bc1ce5c149b44ca29ef
Bhesh Sejawal <bsejawal@gmail.com>
2024-08-02 16:30:28 -0500

    linklist removeLast and prepend value

--- src/main/java/com/bsejawal/algorithms/linklist/LinkedList.java
+++ src/main/java/com/bsejawal/algorithms/linklist/LinkedList.java
@@ -5,48 +5,81 @@ public class LinkedList {
     private Node tail;
     private int length;
 
-    class Node{
+    class Node {
         int value;
         Node next;
-        Node(int value){
+
+        Node(int value) {
             this.value = value;
         }
     }
 
-    public LinkedList(int value){
+    public LinkedList(int value) {
         Node newNode = new Node(value);
         head = newNode;
         tail = newNode;
         length = 1;
     }
 
-    public void printList(){
+    public void printList() {
         Node temp = head;
-        while(temp != null){
+        while (temp != null) {
             System.out.println(temp.value);
             temp = temp.next;
         }
     }
 
-    public void append(int value){
+    public void append(int value) {
         Node newNode = new Node(value);
-        if(length == 0){
+        if (length == 0) {
             head = newNode;
-        }else{
+        } else {
             tail.next = newNode;
         }
         tail = newNode;
         length++;
     }
 
-    public void getHead(){
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = head;
+        Node pre = head;
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+    public void prepend(int value){
+        Node newNode = new Node(value);
+        if(length == 0){
+            head = newNode;
+            tail = newNode;
+        }else{
+            Node temp = head;
+            newNode.next = temp;
+            head = newNode;
+        }
+        length++;
+
+    }
+
+    public void getHead() {
         System.out.println("Head: " + head.value);
     }
 
     public void getTail() {
         System.out.println("Tail: " + tail.value);
     }
-    public void getLength(){
+
+    public void getLength() {
         System.out.println("Length: " + length);
     }
 
--- src/main/java/com/bsejawal/algorithms/linklist/TestLinkList.java
+++ src/main/java/com/bsejawal/algorithms/linklist/TestLinkList.java
@@ -4,13 +4,19 @@ public class TestLinkList {
     public static void main(String[] args) {
 
         LinkedList myLinkedList = new LinkedList(4);
-        myLinkedList.append(2);
-        myLinkedList.append(1);
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getLength();
-
+        myLinkedList.removeLast();
+        System.out.println("-> should be empty ");
         myLinkedList.printList();
+        myLinkedList.prepend(5);
+        System.out.println("-> after");
+        myLinkedList.printList();
+//        myLinkedList.append(2);
+//        myLinkedList.append(1);
+//        myLinkedList.getHead();
+//        myLinkedList.getTail();
+//        myLinkedList.getLength();
+//
+//        myLinkedList.printList();
 
     }
 }
