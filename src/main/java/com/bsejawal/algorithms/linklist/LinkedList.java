eb8da2c6aa5bc7d6c63b47ed499a99d8b1fecccc
Bhesh Sejawal <bsejawal@gmail.com>
2024-08-08 14:46:37 -0500

    checking if linklist has loop

--- src/main/java/com/bsejawal/algorithms/linklist/LinkedList.java
+++ src/main/java/com/bsejawal/algorithms/linklist/LinkedList.java
@@ -161,6 +161,17 @@ public class LinkedList {
         return slow;
     }
 
+    public boolean hasLoop(){
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+
     public void getHead() {
         System.out.println("Head: " + head.value);
     }
